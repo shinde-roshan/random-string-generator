3fca3475824a6717bd9ab59d5bd34f1231baa267
roshan <roshan.shinde164@gmail.com>
2025-03-24 23:39:23 +0530

    RandomStringRepository moved to repository package

--- app/src/main/java/com/example/randomstringgenerator/data/respository/RandomStringRepository.kt
+++ app/src/main/java/com/example/randomstringgenerator/data/respository/RandomStringRepository.kt
@@ -1,6 +1,7 @@
-package com.example.randomstringgenerator.data.source
+package com.example.randomstringgenerator.data.respository
 
 import com.example.randomstringgenerator.data.model.RandomText
+import com.example.randomstringgenerator.data.source.ContentProviderDataSource
 
 class RandomStringRepository(private val dataSource: ContentProviderDataSource) {
     suspend fun getRandomText(length: Int): Result<RandomText> {
