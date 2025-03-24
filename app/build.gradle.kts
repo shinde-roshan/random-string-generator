4267d05b4e6073f0bc973dcc8514a28e6e39276e
roshan <roshan.shinde164@gmail.com>
2025-03-24 23:18:00 +0530

    minCompileSdk 35 required

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.randomstringgenerator"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.randomstringgenerator"
