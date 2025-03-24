8b058ec6396c85a08cf6d5a4be22aa5c6b449de9
roshan <roshan.shinde164@gmail.com>
2025-03-24 21:52:00 +0530

    added Gson library for json conversion

--- app/src/main/java/com/example/randomstringgenerator/data/source/ContentProviderDataSource.kt
+++ app/src/main/java/com/example/randomstringgenerator/data/source/ContentProviderDataSource.kt
@@ -2,9 +2,11 @@ package com.example.randomstringgenerator.data.source
 
 import android.content.ContentResolver
 import android.net.Uri
+import com.google.gson.Gson
 
 class ContentProviderDataSource(private val contentResolver: ContentResolver) {
     private val contentUri: Uri = Uri.parse("content://com.example.contestdataprovider/text")
+    private val gson = Gson()
 
     suspend fun getRandomText(length: Int): String {
         return ""
