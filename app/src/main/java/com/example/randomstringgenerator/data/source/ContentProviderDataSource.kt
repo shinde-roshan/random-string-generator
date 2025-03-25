413668f1e777eb1d6622887a01769f8f8cf0b281
roshan <roshan.shinde164@gmail.com>
2025-03-25 06:54:57 +0530

    content provider read permission declared

--- app/src/main/java/com/example/randomstringgenerator/data/source/ContentProviderDataSource.kt
+++ app/src/main/java/com/example/randomstringgenerator/data/source/ContentProviderDataSource.kt
@@ -8,7 +8,7 @@ import com.example.randomstringgenerator.data.model.RandomTextResponse
 import com.google.gson.Gson
 
 class ContentProviderDataSource(private val contentResolver: ContentResolver) {
-    private val contentUri: Uri = Uri.parse("content://com.example.contestdataprovider/text")
+    private val contentUri: Uri = Uri.parse("content://com.iav.contestdataprovider/text")
     private val gson = Gson()
 
     suspend fun getRandomText(length: Int): Result<RandomText> {
