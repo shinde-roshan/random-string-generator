fd3844e6cf66d390017618dec2fcadaa4f54e64a
roshan <roshan.shinde164@gmail.com>
2025-03-25 11:27:11 +0530

    returning error result if content provider is taking more than 2 sec to get result

--- app/src/main/java/com/example/randomstringgenerator/data/source/ContentProviderDataSource.kt
+++ app/src/main/java/com/example/randomstringgenerator/data/source/ContentProviderDataSource.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import com.example.randomstringgenerator.data.model.RandomText
 import com.example.randomstringgenerator.data.model.RandomTextResponse
 import com.google.gson.Gson
+import kotlinx.coroutines.withTimeout
 
 class ContentProviderDataSource(private val contentResolver: ContentResolver) {
     private val contentUri: Uri = Uri.parse("content://com.iav.contestdataprovider/text")
@@ -13,17 +14,21 @@ class ContentProviderDataSource(private val contentResolver: ContentResolver) {
 
     suspend fun getRandomText(length: Int): Result<RandomText> {
         return try {
-            val queryArgs = Bundle().apply {
-                putInt(ContentResolver.QUERY_ARG_LIMIT, length)
-            }
-            contentResolver.query(contentUri, null, queryArgs, null)?.use { cursor ->
-                if (cursor.moveToFirst()) {
-                    val jsonData = cursor.getString(cursor.getColumnIndex("data"))
-                    val randomTextItem = gson.fromJson(jsonData, RandomTextResponse::class.java)
-                    return Result.success(randomTextItem.randomText)
+            withTimeout(2000)
+            {
+                val queryArgs = Bundle().apply {
+                    putInt(ContentResolver.QUERY_ARG_LIMIT, length)
                 }
+                contentResolver.query(contentUri, null, queryArgs, null)?.use { cursor ->
+                    if (cursor.moveToFirst())
+                    {
+                        val jsonData = cursor.getString(cursor.getColumnIndex("data"))
+                        val randomTextItem = gson.fromJson(jsonData, RandomTextResponse::class.java)
+                        return@withTimeout Result.success(randomTextItem.randomText)
+                    }
+                }
+                Result.failure(Exception("Failed to get string."))
             }
-            Result.failure(Exception("Failed to get string."))
         } catch (e: Exception) {
             Result.failure(e)
         }
